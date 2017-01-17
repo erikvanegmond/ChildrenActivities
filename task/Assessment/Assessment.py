80d8c4ba956a335d7e1cbe451bceab34db269fdd
Erik van Egmond <erikvane@gmail.com>
2017-01-17 14:08:41 +0100

    Add function to select domain

--- task/Assessment/Assessment.py
+++ task/Assessment/Assessment.py
@@ -3,11 +3,15 @@ from child.Child import Child
 
 class Assessment:
     def __init__(self):
-        self.child = Child("bob")
+        self.child = Child("Bob")
 
     def assess_child(self):
         case = self.child.case
         # Loop over the norms
         for norm_name, norm in self.child.norms.norms.items():
             norm.evaluate(case)
+
+
+    def select_domain(self):
+        pass
 
